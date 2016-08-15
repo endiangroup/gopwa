18f3c74a4f7dda5ac4412d39a407e552836bdfbc
Adrian Duke <adrian.duke@gmail.com>
2016-08-15 13:39:05 +0100

    Add PWS endpoints and regions

---
+++ endpoints.go
@@ -0,0 +1,32 @@
+package gopwa
+
+type Region uint8
+
+const (
+	UK Region = iota
+	DE
+	EU
+	US
+	NA
+	JP
+)
+
+var Regions = []string{
+	UK: "mws-eu.amazonservices.com",
+	DE: "mws-eu.amazonservices.com",
+	EU: "mws-eu.amazonservices.com",
+	US: "mws.amazonservices.com",
+	NA: "mws.amazonservices.com",
+	JP: "mws.amazonservices.jp",
+}
+
+const (
+	V20130101 = "2013-01-01"
+)
+
+type Environment string
+
+const (
+	Sandbox Environment = "/OffAmazonPayments_Sandbox"
+	Live                = "/OffAmazonPayments"
+)
